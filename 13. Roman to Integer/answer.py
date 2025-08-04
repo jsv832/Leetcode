cbf5c5b7425ec0b6dbee6f9c065b3d211cdd968c
Jose Saraiva Vaz <jose102@live.com.pt>
2025-08-04 17:49:40 +0100

    Leetcode Q13

---
+++ 13. Roman to Integer/answer.py
@@ -0,0 +1,21 @@
+def romanToInt(self, s: str) -> int:
+    ans = 0
+    convert = {"I":1,
+                "V":5,
+                "X":10,
+                "L":50,
+                "C":100,
+                "D":500,
+                "M":1000}
+
+    s = s.replace("IV", "IIII")
+    s = s.replace("IX", "VIIII")
+    s = s.replace("XL", "XXXX")
+    s = s.replace("XC", "LXXXX")
+    s = s.replace("CD", "CCCC")
+    s = s.replace("CM", "DCCCC")
+
+    for char in s:
+        ans += convert[char]
+        
+    return ans
