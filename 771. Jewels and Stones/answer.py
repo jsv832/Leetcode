7c96f7d09706ece4f46ddb1a48621e600d541a0c
Jose Saraiva Vaz <jose102@live.com.pt>
2025-08-10 14:31:19 +0100

    Leetcode Q217 & Q771

---
+++ 771. Jewels and Stones/answer.py
@@ -0,0 +1,10 @@
+def numJewelsInStones(self, jewels: str, stones: str) -> int:
+    
+    jewels_set = set(jewels)
+    ans = 0
+    
+    for stone in stones:
+        if stone in jewels_set:
+            ans += 1
+
+    return ans
